c2cc80578c8a004b00326513c4d7a9e32abd6e46
Ubuntu <Thomas Weichhart>
2022-11-11 12:19:41 +0000

    removed mdns

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-localIP = '0.0.0.0'
-localIPv6 = '0000::0000:0000:0000:0000'
+ipv4 = '130.162.213.82'
+ipv6 = '0000::0000:0000:0000:0000'
 
 const wsPort = 3001
-const httpPort = 81
-const mdnsPort = 5353
+const httpPort = 80
+// const mdnsPort = 5353
 
-const { exec } = require("child_process");
+// const { exec } = require("child_process");
 const express = require('express')
 const app = express()
 const io = require("socket.io")(wsPort, {
@@ -15,46 +15,46 @@ const io = require("socket.io")(wsPort, {
         allowedHeaders: ['*'],
     }
 })
-const mdns = require('multicast-dns')({
-    reuseAddr: true,
-    noInit: true,
-    port: mdnsPort
-})
+// const mdns = require('multicast-dns')({
+//     reuseAddr: true,
+//     noInit: true,
+//     port: mdnsPort
+// })
 const { instrument } = require("@socket.io/admin-ui");
 
 instrument(io, {
     auth: false
 });
 
-exec("ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/'", (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    localIP = stdout.trim()
-    console.log(`local ip: ${localIP}`)
-});
+// exec("ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/'", (error, stdout, stderr) => {
+//     if (error) {
+//         console.error(`exec error: ${error}`);
+//         return;
+//     }
+//     localIP = stdout.trim()
+//     console.log(`local ip: ${localIP}`)
+// });
 
-exec("ip addr | grep 'state UP' -A4 | tail -n1 | awk '{print $2}' | cut -f1 -d'/'", (error, stdout, stderr) => {
-    if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-    }
-    localIPv6 = stdout.trim()
-    console.log(`local ipv6: ${localIP}`)
-});
+// exec("ip addr | grep 'state UP' -A4 | tail -n1 | awk '{print $2}' | cut -f1 -d'/'", (error, stdout, stderr) => {
+//     if (error) {
+//         console.error(`exec error: ${error}`);
+//         return;
+//     }
+//     localIPv6 = stdout.trim()
+//     console.log(`local ipv6: ${localIP}`)
+// });
 
-mdns.on('query', query => {
-    if (query.questions[0]?.name === 'htlchat.local') {
-        if (query.questions[0].type === 'A') {
-            mdns.respond([{ name: 'htlchat.local', type: 'A', data: localIP }])
-        }
-        if (query.questions[0].type === 'AAAA') {
-            mdns.respond([{ name: 'htlchat.local', type: 'AAAA', data: localIPv6 }])
-        }
-        console.log(query)
-    }
-})
+// mdns.on('query', query => {
+//     if (query.questions[0]?.name === 'htlchat.local') {
+//         if (query.questions[0].type === 'A') {
+//             mdns.respond([{ name: 'htlchat.local', type: 'A', data: ipv4 }])
+//         }
+//         if (query.questions[0].type === 'AAAA') {
+//             mdns.respond([{ name: 'htlchat.local', type: 'AAAA', data: ipv6 }])
+//         }
+//         console.log(query)
+//     }
+// })
 
 io.on("connection", socket => {
     socket.on("message", msg => {
@@ -71,11 +71,11 @@ io.on("connection", socket => {
 })
 
 app.get('/api/ip', (req, res) => {
-    res.send(localIP)
+    res.send(ipv4)
 })
 
 app.get('/api/ipv6', (req, res) => {
-    res.send(localIPv6)
+    res.send(ipv6)
 })
 
 app.use(express.static(__dirname + '/site'))
@@ -84,8 +84,8 @@ app.listen(httpPort, () => {
     console.log(`html\t server listening on port ${httpPort} `)
 })
 
-mdns.on('ready', () => {
-    console.log(`mdns\t server listening on port ${mdnsPort} `)
-})
+// mdns.on('ready', () => {
+//     console.log(`mdns\t server listening on port ${mdnsPort} `)
+// })
 
 console.log(`ws\t server listening on port ${wsPort} `)
